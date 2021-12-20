eeb248ce4db7905b54ae527c36d05cb0e04a6786
konagaya <cresta522@gmail.com>
2021-12-20 14:13:34 +0900

    DDD用ファイル作成

---
+++ src/app/Repositories/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Requests\User\CreateAdminUserRequest;
+
+class UserRepository extends Repository
+{
+
+    public function createAdminUser(CreateAdminUserRequest $request)
+    {
+        // TODO: Implement create() method.
+    }
+}
