215e6d7db001ac7e0d05b1b455f19f3dfb57172b
hosimiya7 <nerokarurin@gmail.com>
2022-01-17 20:39:37 +0900

    blade作成

---
+++ src/resources/views/estimate/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container estimate">
+    <div class="estimate_index">
+        <div class="flex">
+            <p>見積もり一覧</p>
+            <a href="" class="estimate_index_create">新規作成</a>
+        </div>
+
+    </div>
+</div>
+@endsection
