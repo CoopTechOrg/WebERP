2dc23240143b37cb169eb292372f32cee5406c12
Ryww.T <80194285+Rywwwwwww@users.noreply.github.com>
2022-02-02 18:58:45 +0900

    テストユーザ登録 及び テストログイン・ログアウト処理作成

--- src/tests/Feature/LoginTest.php
+++ src/tests/Feature/LoginTest.php
@@ -28,7 +28,7 @@ class Login extends TestCase
 		]);
 	}
 
-	/*=====　パスワード処理　=====*/
+	/*=====　ログイン処理【成功時】　=====*/
 	public function test_正しいパスワードの場合()
 	{
 		
@@ -38,12 +38,53 @@ class Login extends TestCase
 	
 		//ログインする(DBに登録されているユーザを使う)
 		$response = $this->post(route('login'), ['email' => "auth_test@gmail.com", 'password' => 'auth_test_password']);
-		//ログインが成功したら、302を返す
+		//リダイレクトを行うため、302を返す
 		$response->assertStatus(302);
 
 		//処理終了後のパス
 		$response->assertRedirect('/home');
 		//上記のユーザーがログイン認証されているか確認
 		$this->assertAuthenticatedAs($this->user);
+	}
+	
+	/*=====　ログアウト処理【失敗時】　=====*/
+	public function test_謝ったパスワードの場合()
+	{
+		$response = $this->get('/login');
+		//正常状態を返す(200)
+		$response->assertStatus(200);
+
+		//ログインする(パスワードは謝ったものを渡す)
+		$response = $this->post(route('login'), ['email' => "auth_test@gmail.com", 'password' => 'XXXXXXXXXXXXXXX']);
+		//リダイレクトを行うため、302を返す
+		$response->assertStatus(302);
+		
+		//リダイレクト後は、ログインページに戻る
+		$response->assertRedirect('/login');
+		//失敗しているので認証されていないメソッドを渡す
+		$this->assertGuest();
+	}
+
+	/*=====　ログアウト処理 =====*/
+	public function test_ログアウト処理()
+	{
+		//登録したユーザーをログイン状態の作成
+		$response = $this->actingAs($this->user);
+		//ログイン完了後の画面へ移動
+		$response = $this->get('/home');
+		//正常状態を返す
+		$response->assertStatus(200);
+
+		//ログアウト処理実施
+		$this->post('logout');
+		//ログアウト正常終了したら200を返す
+		$response->assertStatus(200);
+		//ログインページに移動
+		$response = $this->get('/login');
+		//移動出来たら正常状態を返す
+		$response->assertStatus(200);
+
+		//ログインしていないので、認証されていないことを確認
+		$this->assertGuest(); 
 	}
 }
