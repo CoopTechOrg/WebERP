c21f4477db616321ca80ed907f7fe3d7830b9864
hosimiya7 <nerokarurin@gmail.com>
2022-02-05 17:27:56 +0900

    型追加

--- src/app/Core/Hash.php
+++ src/app/Core/Hash.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Hash as FacadesHash;
 
 class Hash extends FacadesHash
 {
-    public static function makeToUrl(string $value):string
+    public static function makeToUrl(string $value): string
     {
         return str_replace('/', '', parent::make($value));
     }
