e994bc85de3ed83875d8b414895aa888ac8dfab7
Zuzu0725 <kzmsan63@gmail.com>
2022-02-28 23:44:06 +0900

    companyモデルでHasManyをuseする

--- src/app/Models/Company.php
+++ src/app/Models/Company.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
