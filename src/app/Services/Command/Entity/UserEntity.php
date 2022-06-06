371edb62d7b66a58107642442acbf18bf0040208
hosimiya7 <nerokarurin@gmail.com>
2022-06-06 22:27:28 +0900

    UserEntity作成

--- src/app/Services/Command/Entity/UserEntity.php
+++ src/app/Services/Command/Entity/UserEntity.php
@@ -4,9 +4,49 @@ namespace App\Services\Command\Entity;
 
 use App\Models\User;
 use App\Repositories\UserRepository;
-use Illuminate\Support\Facades\Hash;
+use App\Exceptions\ORMNotFoundException;
 
-class UserEntity
+class UserEntity extends Entity
 {
+    private ?User $user;
 
+    private UserRepository $userRepository;
+
+    public function __construct()
+    {
+        $this->userRepository = app(UserRepository::class);
+    }
+
+    public function getModel(): User
+    {
+        return $this->user;
+    }
+
+    public static function getInstance(?int $id = null): self
+    {
+        $entity = new self();
+        $entity->id = $id;
+
+        $entity->getDefaultModel();
+
+        return $entity;
+    }
+
+    public static function new(): self
+    {
+        return self::getInstance();
+    }
+
+    public static function find(int $id): self
+    {
+        return self::getInstance($id);
+    }
+
+    protected function getDefaultModel(): void
+    {
+        $this->user = $this->id === null ? new User() : $this->userRepository->select($this->id);
+        if($this->user === null) {
+            throw new ORMNotFoundException();
+        }
+    }
 }
