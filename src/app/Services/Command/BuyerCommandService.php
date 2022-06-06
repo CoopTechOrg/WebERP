6f9628fb33d1d6dd35ac0fcf217c180a669424b5
hosimiya7 <nerokarurin@gmail.com>
2022-06-07 00:09:44 +0900

    Buyer store作成

--- src/app/Services/Command/BuyerCommandService.php
+++ src/app/Services/Command/BuyerCommandService.php
@@ -15,9 +15,38 @@ class BuyerCommandService
         $this->buyerRepository = $buyerRepository;
     }
 
-    public function create(string $name, string $unit, ?string $remarks): Buyer
-    {
-        $buyerEntity = new BuyerEntity($name, $unit, $remarks);
+    /**
+     * 販売者情報を新規作成する
+     *
+     * @param string|null $email
+     * @param string|null $tel
+     * @param string $contactPersonFamilyName
+     * @param string $contactPersonGivenName
+     * @param integer $prefectureId
+     * @param string $postalCode
+     * @param string $city
+     * @param string $town
+     * @param string $building
+     * @param string|null $remarks
+     * @return Buyer
+     */
+    public function create(
+        ?string $email,
+        ?string $tel,
+        string $contactPersonFamilyName,
+        string $contactPersonGivenName,
+        int $prefectureId,
+        string $postalCode,
+        string $city,
+        string $town,
+        string $building,
+        ?string $remarks
+    ): Buyer {
+        $buyerEntity = BuyerEntity::new()
+            ->setContact($email, $tel)
+            ->setNames($contactPersonFamilyName, $contactPersonGivenName)
+            ->setAddress($prefectureId, $postalCode, $city, $town, $building)
+            ->setRemarks($remarks);
 
         /**
          * @var Buyer $buyer
--- src/app/Services/Command/Entity/BuyerEntity.php
+++ src/app/Services/Command/Entity/BuyerEntity.php
@@ -10,27 +10,6 @@ use App\Repositories\BuyerRepository;
 
 class BuyerEntity extends Entity
 {
-
-    private ?string $email;
-
-    private ?string $tel;
-
-    private string $contactPersonFamilyName;
-
-    private string $contactPersonGivenName;
-
-    private int $prefectureId;
-
-    private string $postalCode;
-
-    private string $city;
-
-    private string $town;
-
-    private string $building;
-
-    private ?string $remarks;
-
     private ?Buyer $buyer;
 
     private BuyerRepository $buyerRepository;
@@ -40,11 +19,22 @@ class BuyerEntity extends Entity
         $this->buyerRepository = app(BuyerRepository::class);
     }
 
+    /**
+     * modelを取得する
+     *
+     * @return Buyer
+     */
     public function getModel(): Buyer
     {
         return $this->buyer;
     }
 
+    /**
+     * インスタンスを取得する
+     *
+     * @param integer|null $id
+     * @return self
+     */
     public static function getInstance(?int $id = null): self
     {
         $entity = new self();
@@ -55,20 +45,97 @@ class BuyerEntity extends Entity
         return $entity;
     }
 
+    /**
+     * 新しいインスタンスを作成する
+     *
+     * @return self
+     */
     public static function new(): self
     {
         return self::getInstance();
     }
 
+    /**
+     * 同idのインスタンスを取得する
+     *
+     * @param integer $id
+     * @return self
+     */
     public static function find(int $id): self
     {
         return self::getInstance($id);
     }
 
+    /**
+     * 名前を設定する
+     *
+     * @param string $familyName
+     * @param string $givenName
+     * @return self
+     */
+    public function setNames(string $familyName, string $givenName): self
+    {
+        $this->buyer->contact_person_family_name = $familyName;
+        $this->buyer->contact_person_given_name = $givenName;
+        return $this;
+    }
+
+    /**
+     * 連絡先を設定する
+     *
+     * @param string|null $email
+     * @param string|null $tel
+     * @return self
+     */
+    public function setContact(?string $email, ?string $tel): self
+    {
+        $this->buyer->email = $email;
+        $this->buyer->tel = $tel;
+        return $this;
+    }
+
+    /**
+     * 住所を設定する
+     *
+     * @param integer $prefectureId
+     * @param string $postalCode
+     * @param string $city
+     * @param string $town
+     * @param string $building
+     * @return self
+     */
+    public function setAddress(int $prefectureId, string $postalCode, string $city, string $town, string $building): self
+    {
+        $this->buyer->prefecture_id = $prefectureId;
+        $this->buyer->postal_code = $postalCode;
+        $this->buyer->city = $city;
+        $this->buyer->town = $town;
+        $this->buyer->building = $building;
+
+        return $this;
+    }
+
+    /**
+     * 備考を設定する
+     *
+     * @param string|null $remarks
+     * @return self
+     */
+    public function setRemarks(?string $remarks): self
+    {
+        $this->buyer->remarks = $remarks;
+        return $this;
+    }
+
+    /**
+     * modelを取得する
+     *
+     * @return void
+     */
     protected function getDefaultModel(): void
     {
         $this->buyer = $this->id === null ? new Buyer() : $this->buyerRepository->select($this->id);
-        if($this->buyer === null) {
+        if ($this->buyer === null) {
             throw new ORMNotFoundException();
         }
     }
