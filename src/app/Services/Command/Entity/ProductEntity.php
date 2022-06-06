58a37d7ed4299329dfac32b6a408ccd43fdf3012
hosimiya7 <nerokarurin@gmail.com>
2022-06-06 21:58:53 +0900

    productEntity作成

--- src/app/Services/Command/Entity/ProductEntity.php
+++ src/app/Services/Command/Entity/ProductEntity.php
@@ -3,29 +3,55 @@
 namespace App\Services\Command\Entity;
 
 use App\Models\Product;
+use App\Enum\ProgramLogicType;
+use App\Exceptions\ORMNotFoundException;
+use App\Exceptions\ProgramLogicException;
+use App\Repositories\ProductRepository;
 
 class ProductEntity extends Entity
 {
 
-    private string $name;
+    private ?Product $product;
 
-    private string $unit;
+    private ProductRepository $productRepository;
 
-    private ?string $remarks;
-
-    public function __construct(string $name, string $unit, ?string $remarks)
+    public function __construct()
     {
-        $this->name = $name;
-        $this->unit = $unit;
-        $this->remarks = $remarks;
+        $this->productRepository = app(ProductRepository::class);
+
     }
 
     public function getModel(): Product
     {
-        $product = new Product();
-        $product->name = $this->name;
-        $product->unit = $this->unit;
-        $product->remarks = $this->remarks;
-        return $product;
+        return $this->product;
     }
+
+    public static function getInstance(?int $id = null): self
+    {
+        $entity = new self();
+        $entity->id = $id;
+
+        $entity->getDefaultModel();
+
+        return $entity;
+    }
+
+    public static function new(): self
+    {
+        return self::getInstance();
+    }
+
+    public static function find(int $id): self
+    {
+        return self::getInstance($id);
+    }
+
+    protected function getDefaultModel(): void
+    {
+        $this->product = $this->id === null ? new Product() : $this->productRepository->select($this->id);
+        if($this->product === null) {
+            throw new ORMNotFoundException();
+        }
+    }
+
 }
